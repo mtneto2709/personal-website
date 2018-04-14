970190d0c51988d956faeec4dde8f2b0a3acb704
messiaslima <messiaslima.03@gmail.com>
2018-04-13 22:06:33 -0300

    email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+	contato: any = {};
+	carregando: boolean = false;
+	erroEmail: boolean = false;
+
 	downloadCV() {
 		window.open("./assets/docs/cv.pdf")
+	}
+
+	openLink(link:string){
+		window.open(link, "blank_");
+	}
+
+	openEmail(){
+		window.open("mailto:messiaslima.03@gmail.com", "_self") ;
+	}
+
+	enviarEmail(contato:any){
+		
 	}
 }
